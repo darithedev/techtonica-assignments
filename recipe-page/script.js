be6f9849b7fd2080ed9a7884817128a811d43919
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-11-07 23:42:56 -0500

    feat(script): Add checkboxes to ingredient list ingredients
    * Added checkbox with label to each ingredient item
    * When a checkbox is checked, visual check box changes

--- recipe-page/script.js
+++ recipe-page/script.js
@@ -1,2 +1,31 @@
 const ingredients = document.querySelector('ul.ingredients');
 const list = ingredients.querySelectorAll('li');
+
+for (let i = 0; i < list.length; i++) {
+    const text = list[i].textContent;
+    
+    list[i].textContent = '';
+    
+    let checkbox = document.createElement("input");
+    
+    checkbox.type = "checkbox";
+    checkbox.name = "ingredient";
+    checkbox.value = text;
+    
+    let label = document.createElement("label");
+    
+    label.appendChild(checkbox);
+    label.appendChild(
+        document.createTextNode(text)
+    );
+
+    list[i].appendChild(label);
+    
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            label.classList.add('checked');
+        } else {
+            label.classList.remove('checked');
+        }
+    });
+}
