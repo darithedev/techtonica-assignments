d71b52d419f5c484d05724b6e8545c3219ef41fd
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-11-07 23:54:49 -0500

    feat(script): Add checkboxes to recipe instructions list
    * Added checkbox with label to each instruction item
    * When a checkbox is checked, visual check box changes

--- recipe-page/script.js
+++ recipe-page/script.js
@@ -32,3 +32,32 @@ for (let i = 0; i < list.length; i++) {
 
 const instructions = document.querySelector('ol');
 const listInst = instructions.querySelectorAll('li');
+
+for (let i = 0; i < listInst.length; i++) {
+    const textInst = listInst[i].textContent;
+    
+    listInst[i].textContent = '';
+    
+    let checkbox = document.createElement("input");
+    
+    checkbox.type = "checkbox";
+    checkbox.name = "instruction";
+    checkbox.value = textInst;
+    
+    let label = document.createElement("label");
+    
+    label.appendChild(checkbox);
+    label.appendChild(
+        document.createTextNode(textInst)
+    );
+
+    listInst[i].appendChild(label);
+
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            label.classList.add('checked');
+        } else {
+            label.classList.remove('checked');
+        }
+    });
+}
