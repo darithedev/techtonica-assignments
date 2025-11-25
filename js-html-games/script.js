7eae2cd79e234ad56c6d2e98295f04c2c0ce1247
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-11-24 23:48:37 -0500

    Add event listener for flip

--- js-html-games/script.js
+++ js-html-games/script.js
@@ -1,3 +1,7 @@
 const coin = document.getElementById('coin');
 const flip = document.getElementById('flip');
 const outcome = document.getElementById('outcome');
+
+flip.addEventListener('click', function () {
+
+});
