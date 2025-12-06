d8cf31698819bcb505044736b9f8e301d4fab753
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-12-05 20:27:20 -0500

    Add project variables for timer, score, board, board size, submit and reset by getting element by id

---
+++ js-html-games/boggle-game/script.js
@@ -0,0 +1,15 @@
+const timer = document.getElementById('timer');
+const score = document.getElementById('score');
+
+const boardSetup = document.getElementById('boardSetup')
+const board = document.getElementById('board');
+const showBoard = document.getElementById('showBoard');
+
+const boardSize = document.getElementById('input');
+
+const standard = document.getElementById('standard');
+const bigBoggle = document.getElementById('bigBoggle');
+const superBoggle = document.getElementById('superBoggle');
+
+const submit = document.getElementById('submit');
+const reset = document.getElementById('reset');
