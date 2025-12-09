13a03b0fc68b39a0fe400379da8cae3df6ff0eec
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-12-09 00:14:09 -0500

    Add tic-tac-toe game and supporting files

---
+++ react-game/cozy-game/src/component/Reset.jsx
@@ -0,0 +1,13 @@
+function Reset () {
+
+    return (
+        <div>
+            <button
+                onClick=""
+            >
+
+            </button>
+        </div>
+    )
+}
+export default Reset;
