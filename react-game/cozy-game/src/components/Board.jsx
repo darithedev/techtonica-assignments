e43595c1e0b23c780faebfbc38b66bdb53a48b6d
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-12-18 14:51:47 -0500

    Add land div and sand color, width and height styling to the game board

--- react-game/cozy-game/src/components/Board.jsx
+++ react-game/cozy-game/src/components/Board.jsx
@@ -4,7 +4,9 @@ function Board ({ level }) {
 
     return (
         <div className="container">
-            <div className="game-board"></div>
+            <div className="game-board">
+                <div className='land'></div>
+            </div>
             <p className="level">You are on level: {level} </p>
         </div>
     )
