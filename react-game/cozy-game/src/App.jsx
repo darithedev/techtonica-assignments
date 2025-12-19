5afc100685532f44c9a3e71120aae6d2205e1c1e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-12-19 11:05:22 -0500

    Add ternary operator to character to render character selection until selection then start game

--- react-game/cozy-game/src/App.jsx
+++ react-game/cozy-game/src/App.jsx
@@ -10,10 +10,19 @@ function App() {
   return (
     <>
       <h1>Capture the Chicks Game</h1>
-      <Board 
-        character={character} 
-        level={level}
-      />
+      {!character ? (
+        <>
+          <p>Choose your main character: </p>
+          {characters.map((c, i) => (
+          <button className="character-button" key={i} onClick={() => setCharacter(c)}>{c}</button>
+          ))}
+        </>
+      ) : (
+        <Board 
+          level={level} 
+          sprite={character} 
+        />
+      )}
     </>
   )
 }
