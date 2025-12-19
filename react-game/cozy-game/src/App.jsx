235591566cc80432bae61bd8b1800a918905057f
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-12-19 11:43:30 -0500

    Add ternary conditional to into message where it is shown until game starts

--- react-game/cozy-game/src/App.jsx
+++ react-game/cozy-game/src/App.jsx
@@ -6,19 +6,22 @@ function App() {
   const characters = ["🦆","🦩","🦢"];
   const [character, setCharacter] = useState("");
   const [level, setLevel] = useState(1);
+  const [intro, isIntroShown] = useState(true);
 
   return (
     <>
       <h1>Capture the Chicks Game</h1>
+      {intro && (
+        <div className='intro-message'>
+          <p>Oh no! Your baby chicks have scattered across multiple levels.</p>
+          <p>Quick! Get your baby chicks before the crocodile gets them!</p>
+        </div>
+      )}
       {!character ? (
         <>
-          <div className='intro-message'>
-            <p>Oh no! Your baby chicks have scattered across multiple levels.</p>
-            <p>Quick! Get your baby chicks before the crocodile gets them!</p>
-          </div>
           <p>Choose your main character: </p>
           {characters.map((c, i) => (
-          <button className="character-button" key={i} onClick={() => setCharacter(c)}>{c}</button>
+          <button className="character-button" key={i} onClick={() => (setCharacter(c), isIntroShown(false))}>{c}</button>
           ))}
         </>
       ) : (
