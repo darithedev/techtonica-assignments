8f3774ec82ac1f62e0483b3da38e18623decab82
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-12-19 11:16:49 -0500

    Add intro message and styling

--- react-game/cozy-game/src/App.jsx
+++ react-game/cozy-game/src/App.jsx
@@ -12,6 +12,10 @@ function App() {
       <h1>Capture the Chicks Game</h1>
       {!character ? (
         <>
+          <div className='intro-message'>
+            <p>Oh no! Your baby chicks have scattered across multiple levels.</p>
+            <p>Quick! Get your baby chicks before the crocodile gets them!</p>
+          </div>
           <p>Choose your main character: </p>
           {characters.map((c, i) => (
           <button className="character-button" key={i} onClick={() => setCharacter(c)}>{c}</button>
