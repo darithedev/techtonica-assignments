f1d822f7b28695fe4332381baabb484a873072a5
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-01-09 00:02:12 -0500

    Add /books endpoint

--- rest-api-project/backend/index.js
+++ rest-api-project/backend/index.js
@@ -9,6 +9,10 @@ const PORT = 3000;
 // Middleware: Allow the server to read JSON data
 app.use(express.json());
 
+app.get('/books', (req, res) => {
+    res.json(BOOKS);
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
