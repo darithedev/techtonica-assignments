ff9c4567076d75f499aaff971348b8ea5a4c4d08
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-23 21:51:29 -0400

    Add players router skeleton

---
+++ react-game/backend/src/routes/players.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import pool from '../db/pools.js'
+
+const router = express.Router();
+
+router.get('/', async(req, res) => {
+
+});
+
+router.post('/', async(req, res) => {
+
+});
+
+router.get('/:id', async(req, res) => {
+
+});
+
+router.put('/:id', async(req, res) => {
+
+});
+
+router.delete('/:id', async(req, res) => {
+
+});
+
+export default router;
