47220169919ab0efe199f1ef6adf1115cbaadc99
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-26 01:06:39 -0400

    Add GET route for players

--- react-game/backend/src/routes/players.js
+++ react-game/backend/src/routes/players.js
@@ -4,7 +4,13 @@ import pool from '../db/pools.js'
 const router = express.Router();
 
 router.get('/', async(req, res) => {
-
+    try {
+        const result = await pool.query(`SELECT * FROM players`);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error with getting all players', error);
+        res.status(500).json({ error: 'Error! Could not get players' });
+    }
 });
 
 router.post('/', async(req, res) => {
