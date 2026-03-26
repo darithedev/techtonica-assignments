1d4769f65ebde87b29d8a45dba1406dde10adaee
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-26 17:55:04 -0400

    Add POST games router

--- react-game/backend/src/routes/games.js
+++ react-game/backend/src/routes/games.js
@@ -14,7 +14,32 @@ router.get('/', async(req, res) => {
 });
 
 router.post('/', async(req, res) => {
+    try {
+        const { player_id, player_score } = req.body;
 
+        if (!player_score || !player_id){
+            return res.status(400).json({
+                error: "A player id and score is required!"
+            });
+        }
+
+        if (typeof(player_score) !== 'number') {
+            return res.status(400).json({
+                error: "A valid score value is required!"
+            });
+        }
+
+        const result = await pool.query(
+            `INSERT INTO games (player_id, player_score)
+            VALUES ($1, $2)
+            RETURNING *`,
+            [player_id, player_score]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error with adding the score for this player.', error);
+        res.status(500).json({ error: 'Error! Could not add the score for this player.' });
+    }
 });
 
 router.get('/:id', async(req, res) => {
