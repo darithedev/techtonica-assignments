b1b839ac8047843213ad7307ee5f7bedb55b4b9e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-26 01:27:33 -0400

    Add POST players route

--- react-game/backend/src/routes/players.js
+++ react-game/backend/src/routes/players.js
@@ -14,7 +14,32 @@ router.get('/', async(req, res) => {
 });
 
 router.post('/', async(req, res) => {
+    try {
+        const { player_name, email, player_username } = req.body;
 
+        if (!player_name || !email || !player_username){
+            return res.status(400).json({
+                error: "A name, email, and player user name tag is required!"
+            });
+        }
+
+        if (!email.includes('@')) {
+            return res.status(400).json({
+                error: "A valid email is required!"
+            });
+        }
+
+        const result = await pool.query(
+            `INSERT INTO players (player_name, email, player_username)
+            VALUES ($1, $2, $3)
+            RETURNING *`,
+            [player_name, email, player_username]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error with adding this player.', error);
+        res.status(500).json({ error: 'Error! Could not add this player.' });
+    }
 });
 
 router.get('/:id', async(req, res) => {
