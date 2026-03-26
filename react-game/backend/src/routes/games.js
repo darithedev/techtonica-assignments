742fd0afad55cd2d643a69d5de8409aee59163da
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-26 18:11:05 -0400

    Add GET router for /games/:id endpoint

--- react-game/backend/src/routes/games.js
+++ react-game/backend/src/routes/games.js
@@ -49,7 +49,34 @@ router.post('/', async(req, res) => {
 });
 
 router.get('/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
 
+        if (id === null) {
+            return res.status(400).json({
+                error: 'Could not find user.'
+            });
+        }
+
+        const result = await pool.query(
+            `SELECT games.id, games.player_score, players.player_username
+            FROM games 
+            JOIN players ON games.player_id = players.id
+            WHERE games.id = $1`,
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(400).json({
+                error: 'Error! This individual game for this user does not exist.'
+            });
+        };
+
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error with getting individual player game score for this game.', error);
+        res.status(500).json({ error: 'Error! Could not get individual player game score for this game.' });
+    };
 });
 
 router.put('/:id', async(req, res) => {
