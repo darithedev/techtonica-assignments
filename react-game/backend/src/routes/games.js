d931f87ee92a6ff5271bf2ecd53cb667a0e0b556
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-26 18:12:28 -0400

    Refactor games router and remove PUT and DELETE routes as they are no longer needed

--- react-game/backend/src/routes/games.js
+++ react-game/backend/src/routes/games.js
@@ -79,12 +79,4 @@ router.get('/:id', async(req, res) => {
     };
 });
 
-router.put('/:id', async(req, res) => {
-
-});
-
-router.delete('/:id', async(req, res) => {
-
-});
-
 export default router;
