cabe44bbcb48e5ca4f31977c3ee0e94685d740d5
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-26 17:28:22 -0400

    Add GET router for games

--- react-game/backend/src/routes/games.js
+++ react-game/backend/src/routes/games.js
@@ -4,7 +4,13 @@ import pool from '../db/pools.js'
 const router = express.Router();
 
 router.get('/', async(req, res) => {
-
+    try {
+        const result = await pool.query(`SELECT * FROM games`);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error with getting all games', error);
+        res.status(500).json({ error: 'Error! Could not get games list.' });
+    }
 });
 
 router.post('/', async(req, res) => {
