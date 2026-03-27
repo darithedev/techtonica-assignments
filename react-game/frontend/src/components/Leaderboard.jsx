6b0046b1966b8536422eb606972ff4e13ba1c7b6
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-27 16:52:32 -0400

    Add Leaderboard component skeleton

---
+++ react-game/frontend/src/components/Leaderboard.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+
+const Leaderboard = ({ isLeaderboardShown }) => {
+
+    return (
+        <div className="leaderboard-container">
+            
+        </div>
+    )
+}
+
+export default Leaderboard;
