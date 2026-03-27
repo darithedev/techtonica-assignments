10f9864dc34b2cc866f9d86566f9ec5bcf83c14a
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-27 16:57:35 -0400

    Add getLeaderboard fetch to get top 10 players list from backend

--- react-game/frontend/src/components/Leaderboard.jsx
+++ react-game/frontend/src/components/Leaderboard.jsx
@@ -4,6 +4,14 @@ import './Leaderboard.css'
 const Leaderboard = ({ isLeaderboardShown }) => {
     const [leaderboardList, setLeaderboardList] = useState([]);
 
+    const getLeaderboard = () => {
+        fetch("http://localhost:3000/api/players/leaderboard")
+        .then((response) => response.json())
+        .then((players) => {
+            setLeaderboardList(players);
+        });
+    };
+
     return (
         <div className="leaderboard-container">
             
